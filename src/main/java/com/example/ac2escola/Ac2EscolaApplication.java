f6df9d59da664f0b374565b306287bcb7d2a90ab
vitorbizarra <vitorbizarracontato@gmail.com>
2024-05-19 19:14:22 -0300

    :card_file_box: raw: Cadastrando agenda

--- src/main/java/com/example/ac2escola/Ac2EscolaApplication.java
+++ src/main/java/com/example/ac2escola/Ac2EscolaApplication.java
@@ -1,13 +1,17 @@
 package com.example.ac2escola;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.ac2escola.controllers.AgendaController;
 import com.example.ac2escola.controllers.CursoController;
 import com.example.ac2escola.controllers.ProfessorController;
+import com.example.ac2escola.dto.agenda.AgendaDTO;
 import com.example.ac2escola.dto.curso.CursoDTO;
 import com.example.ac2escola.dto.curso.DadosCursoDTO;
 import com.example.ac2escola.dto.professor.DadosProfessorDTO;
@@ -18,8 +22,9 @@ public class Ac2EscolaApplication {
 
 	@Bean
 	public CommandLineRunner init(
-			@Autowired ProfessorController professorController,
-			@Autowired CursoController cursoController) {
+			@Autowired AgendaController agendaController,
+			@Autowired CursoController cursoController,
+			@Autowired ProfessorController professorController) {
 		return args -> {
 
 			ProfessorDTO professorDTO = new ProfessorDTO(
@@ -45,11 +50,22 @@ public class Ac2EscolaApplication {
 					dadosCursoDTO.getId(),
 					dadosProfessorDTO.getId());
 
+			AgendaDTO agendaDTO = new AgendaDTO(
+					null,
+					dadosCursoDTO.getId(),
+					dadosProfessorDTO.getId(),
+					LocalDate.now(),
+					LocalDate.now().plusMonths(1),
+					"Sorocaba",
+					"SP",
+					"18000-000",
+					"Resumo");
+
+			agendaController.create(agendaDTO);
 		};
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ac2EscolaApplication.class, args);
 	}
-
 }
